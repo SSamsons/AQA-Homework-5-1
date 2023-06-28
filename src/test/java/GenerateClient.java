63489d6d28104fbb5ec24655762475284386b28f
Stanislav <people.ss@yandex.ru>
2023-06-28 19:41:54 +0300

    initial commit

--- src/test/java/GenerateClient.java
+++ src/test/java/GenerateClient.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class GenerateClient {
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class GenerateClient {
+    private GenerateClient() {
+    }
+
+        public static String setDate ( int addDays){
+            String date = LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+            return date;
+        }
+
+        public static String getCity (String locale){
+            Faker faker = new Faker(new Locale("ru"));
+            String city = faker.address().city();
+            return city;
+        }
+
+        public static String getName (String locale){
+            Faker faker = new Faker(new Locale("ru"));
+            String name = faker.name().fullName();
+            return name;
+        }
+
+        public static String getPhoneNumber (String locale){
+            Faker faker = new Faker(new Locale(locale));
+            String phoneNumber = faker.phoneNumber().phoneNumber();
+            return phoneNumber;
+        }
+
+        public static class Registration {
+            private Registration() {
+            }
+
+            public static UserInfo generateUser(String locale) {
+                return new UserInfo(getCity(locale), getName(locale), getPhoneNumber(locale));
+            }
+        }
+
+        @Value
+        public static class UserInfo {
+        String city;
+        String name;
+        String phoneNumber;
+    }
 }
--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -1,31 +1,45 @@
-import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Condition.*;
 
-public class RegistrationTest { ;
-    public String setDate(int addDays) {
-        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
+public class RegistrationTest {
+
 
     @Test
-    void shouldRegisterByAccountNumberDOMModification() {
-        String currentDate = setDate(5);
-        Configuration.holdBrowserOpen = true;
+    public void shouldTestingTheQuestionnaireForOrderingMeeting() {
+        var validationTest = GenerateClient.Registration.generateUser("ru");
+        var daysAddFirst = 5;
+        var firstDate = GenerateClient.setDate(daysAddFirst);
+        var daysAddSecond = 6;
+        var secondDate = GenerateClient.setDate(daysAddSecond);
         open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Санкт-Петербург");
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.BACK_SPACE));
-        $("[data-test-id='date'] input").setValue(currentDate);
-        $("[data-test-id='name'] input").setValue("Самсонов Станислав");
-        $("[data-test-id='phone'] input").setValue("+79363737373");
+        $("[data-test-id='city'] input").setValue(validationTest.getCity()).sendKeys(Keys.chord(Keys.TAB));
+        $("[data-test-id='date'] input").sendKeys( Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(firstDate);
+        $("[data-test-id='name'] input").setValue(validationTest.getName());
+        $("[data-test-id='phone'] input").setValue(validationTest.getPhoneNumber());
         $("[data-test-id='agreement']").click();
-        $$("[type=button]").filter(visible).get(1).click();
-        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
+        $$("[type=button]").filter(Condition.visible).get(1).click();
+        $("[data-test-id=success-notification]").shouldBe(visible, Duration.ofSeconds(5));
+        $(".notification__content").shouldBe(text("Встреча успешно запланирована на " + firstDate))
+                .shouldBe(visible);
+
+        $("[data-test-id='date'] input").doubleClick().sendKeys( Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(secondDate);
+        $$("[type=button]").filter(Condition.visible).get(1).click();
+        $(withText("Необходимо подтверждение")).shouldBe(Condition.visible, Duration.ofSeconds(5));
+        $(withText("У вас уже запланирована встреча на другую дату. Перепланировать?"))
+                .shouldBe(Condition.visible, Duration.ofSeconds(5));;
+        $("[data-test-id= replan-notification] button").click();
+        $("[data-test-id=success-notification]").shouldBe(visible, Duration.ofSeconds(5));
+        $(".notification__content").shouldBe(text("Встреча успешно запланирована на " + secondDate))
+                .shouldBe(visible);
     }
 }
